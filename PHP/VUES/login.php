1bec44eb29a41ad14ab43f3a4081e838d7fa4468
oussamaamou <amoussdark@gmail.com>
2024-12-26 17:27:44 +0100

    Regler la page de connexion

--- PHP/VUES/login.php
+++ PHP/VUES/login.php
@@ -6,12 +6,12 @@
     $error_message = ""; 
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $email = $_POST['email'];
+        $username = $_POST['username'];
         $password = $_POST['password'];
 
         
         if ($result === false) {
-            $error_message = "Mot de Passe ou Email est incorrect!";
+            $error_message = "Mot de Passe ou Username est incorrect!";
         }
     }
 
@@ -44,8 +44,8 @@
                             </h1>
                             <form class="space-y-4 md:space-y-6" method="POST">
                                 <div>
-                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre Email</label>
-                                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="rachad@gmail.com" required="">
+                                    <label for="username" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre Username</label>
+                                    <input type="text" name="username" id="username" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="rachad@gmail.com" required="">
                                 </div>
                                 <div>
                                     <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre Password</label>
